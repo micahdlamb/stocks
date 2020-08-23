3f8125a04c8638186e107aba6c6e48d67a31321a
Micah <micah.d.lamb@gmail.com>
2020-08-22 23:57:45 -0400

    everything works! except the predictions

--- fetch_stock_data.py
+++ fetch_stock_data.py
@@ -33,18 +33,24 @@ for i, link in enumerate(ticker_links):
 
     print(f"Downloading {ticker} {i}/{len(ticker_links)}...", end='')
 
-    try:
+    def get(url):
+        for i in range(5):
+            try:
+                resp = requests.get(url)
+                resp.raise_for_status()
+                return resp
+            except requests.HTTPError as error:
+                print(f"{error}...", end='')
+                time.sleep(10 * i)
 
+    try:
         def scrape_data(url):
-            for i in range(5):
-                try:
-                    html = requests.get(url).text
-                    break
-                except:
-                    time.sleep(10)
+            html = get(url).text
             start = html.index('var originalData = ')
             stop = html.index("]", start)
-            return json.loads(html[start+len('var originalData = '):stop+1])
+            data = html[start+len('var originalData = '):stop+1]
+            if data.startswith('null'): raise LookupError("data is null")
+            return json.loads(data)
 
         key_financial_ratios = scrape_data(f"https://www.macrotrends.net/stocks/charts/{link}/financial-ratios?freq=Q")
         income_statement = scrape_data(f"https://www.macrotrends.net/stocks/charts/{link}/income-statement?freq=Q")
@@ -58,23 +64,13 @@ for i, link in enumerate(ticker_links):
             if '<' in name:
                 name = re.findall(">(.+?)<", col['field_name'])[0]
             name = name.split(' - ', 1)[0].replace(' ', '_').replace('-', '_').replace('/', '2').lower()
-            values = [flt(val) if val != '-' else None for key, val in col.items() if key[0].isdigit()]
+            values = [flt(col[date]) for date in dates]
             return name, values
 
-        dates = [datetime.strptime(key, "%Y-%m-%d") for key, val in key_financial_ratios[0].items() if key[0].isdigit()]
+        dates = [key for key, val in key_financial_ratios[0].items() if key[0].isdigit()]
         key_financial_ratios = dict(get_values(col) for col in key_financial_ratios)
         income_statement = dict(get_values(col) for col in income_statement)
-
-        # def get_price(date):
-        #     for i in range(5):
-        #         stop = date + timedelta(days=3 + i*20)
-        #         sys.stdout = io.StringIO()
-        #         prices = yf.download(ticker, date.strftime("%Y-%m-%d"), stop.strftime("%Y-%m-%d"))
-        #         sys.stdout = sys.__stdout__
-        #         if (len(prices)): return prices['Open'][0]
-        #         print('retry...', end='')
-        #     else:
-        #         print('fail...', end='')
+        dates = [datetime.strptime(date, "%Y-%m-%d") for date in dates]
 
         with hide_print():
             all_prices = yf.download(ticker, start=dates[-1])
@@ -91,13 +87,6 @@ for i, link in enumerate(ticker_links):
                 d = date + timedelta(days=i)
                 try: return price_map[d.strftime("%Y-%m-%d")]
                 except: pass
-
-            # try:
-            #     _date, row = next((_date, row) for _date, row in all_prices.iterrows() if _date > date)
-            #     assert abs(_date - date) < timedelta(days=7), (_date - date, _date, date)
-            #     return row['Open']
-            # except:
-            #     return None
 
         prices    = [get_price(date) for date in dates]
         prices_1m = [get_price(date + timedelta(days=30)) for date in dates]
@@ -127,15 +116,10 @@ for i, link in enumerate(ticker_links):
         data = data.append(df)
 
     data.to_pickle('stock_data_downloading.pickle')
-    print(f"Done!")
+    print(f"done!")
+
 
 print(', '.join(data.columns))
-
-data = data.dropna(subset='price_1y'.split())
-data['earnings2price'] = data['eps'] / data['price']
-data['book2price'] = data['book_value_per_share'] / data['price']
-
-data = data['ticker date price price_1m price_6m price_1y market sector industry net_income revenue research_and_development_expenses earnings2price book2price current_ratio long_term_debt_2_capital debt2equity_ratio total_non_operating_income2expense'.split()]
 data = data.convert_dtypes()
-
+data = data['ticker date price price_1m price_6m price_1y market sector industry net_income revenue research_and_development_expenses eps book_value_per_share current_ratio long_term_debt_2_capital debt2equity_ratio total_non_operating_income2expense'.split()]
 data.to_pickle('stock_data.pickle')
